74d7cece28b2c235de38868b86e7e413f1d29ae0
Elliott He <elliott.he@outlook.com>
2022-01-28 16:34:03 +0800

    encode packed

--- utils/src/abi.rs
+++ utils/src/abi.rs
@@ -5,12 +5,14 @@ pub enum SolType<'a> {
     Str(&'a str),
     Bytes(&'a [u8]),
     // fixed solidity length, but not in other language
-    Bytes32(&'a [u8]), // len should be 32
-    Addr(&'a [u8]), // len should be 20, use addr to avoid confusion w/ cosmwasm Address
-    Uint256(&'a [u8]), // will left pad to 32 bytes, [u8] is big.Int.Bytes()
+    Bytes32(&'a [u8; 32]), // len should be 32
+    Addr(&'a [u8; 20]), // len should be 20, use addr to avoid confusion w/ cosmwasm Address
+    Uint256(&'a [u8; 32]), // will left pad to 32 bytes, [u8] is big.Int.Bytes()
     // array type, we have to treat it differently due to encode packed pad array element
-    AddrList(Vec<Vec<u8>>),
+    AddrList(&'a Vec<[u8; 20]>),
 }
+
+pub type Word = [u8; 32];
 
 pub fn encode_packed(items: &[SolType]) -> Vec<u8> {
     let raw = items.iter().fold(Vec::new(), |mut acc, i| {
@@ -24,6 +26,55 @@ pub fn encode_packed(items: &[SolType]) -> Vec<u8> {
 
 pub fn pack<'a>(data: &'a SolType) -> Vec<u8> {
     let mut raw = Vec::new();
-    raw
+    match data {
+        SolType::Str(s) => {
+            raw.extend(s.as_bytes());
+        }
+        SolType::Bytes(b) => {
+            raw.extend(*b);
+        }
+        SolType::Bytes32(b) => {
+            raw.extend(*b);
+        }
+        SolType::Addr(a) => {
+            raw.extend(*a);
+        }
+        SolType::Uint256(n) => {
+            raw.extend(*n);
+        }
+        SolType::AddrList(a) => {
+            let encoded = a.iter().fold(Vec::new(), |mut acc, i| {
+                let padded = pad_addr(i).to_vec();
+                acc.push(padded);
+                acc            
+            });
+            let encoded = encoded.join(&[][..]);
+            raw.extend(encoded);
+        }
+    };
+    return raw;
 }
+
+fn pad_addr(addr: &[u8; 20]) -> Word {
+    let mut padded = [0u8; 32];
+    padded[12..].copy_from_slice(addr);
+    padded
+}
+
+
+#[cfg(test)]
+mod tests {
+    use hex_literal::hex;
+    use super::*;
+
+    #[test]
+	fn encode_addr_list() {
+		let addresses = vec![hex!("1111111111111111111111111111111111111111"), hex!("1111111111111111111111111111111111111112")];
+        let addr_list = SolType::AddrList(&addresses);
+		let encoded = encode_packed(&[addr_list]);
+		let expected = hex!("00000000000000000000000011111111111111111111111111111111111111110000000000000000000000001111111111111111111111111111111111111112 ").to_vec();
+		assert_eq!(encoded, expected);
+	}
+}
+
 
