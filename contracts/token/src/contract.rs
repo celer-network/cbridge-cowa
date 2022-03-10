f63a979b9fe4b0077fb0a5b855906e957dc6d067
ClaudeZsb <baobaobao369@163.com>
2022-03-10 19:26:12 +0800

    revert test change

--- contracts/token/src/contract.rs
+++ contracts/token/src/contract.rs
@@ -1,4 +1,4 @@
-//#[cfg(not(feature = "library"))]
+#[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128, Addr};
 use cw2::set_contract_version;
