0515042dc6f6baa02ac0d72f1adf43c0cc19aea7
Elliott He <elliott.he@outlook.com>
2022-03-14 12:58:03 +0800

    fix bug

--- contracts/bridge/src/msg.rs
+++ contracts/bridge/src/msg.rs
@@ -32,5 +32,5 @@ pub enum QueryMsg {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum MigrateMsg {
+pub struct MigrateMsg {
 }
