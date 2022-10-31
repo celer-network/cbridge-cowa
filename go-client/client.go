afbe280913c3a2f9af16b43e9205bf7229974f3b
Elliott He <elliott.he@outlook.com>
2022-10-31 16:02:08 +0800

    go client

---
+++ go-client/client.go
@@ -0,0 +1,285 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/celer-network/goutils/log"
+	"github.com/celer-network/sgn-v2/common"
+	commontypes "github.com/celer-network/sgn-v2/common/types"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	tmclient "github.com/cosmos/ibc-go/v2/modules/light-clients/07-tendermint/types"
+	ec "github.com/ethereum/go-ethereum/common"
+	lens "github.com/strangelove-ventures/lens/client"
+	tmquery "github.com/tendermint/tendermint/libs/pubsub/query"
+)
+
+// all in one helper to do everything about cosmos chain
+type CosClient struct {
+	ChainID    uint64
+	RawChainID string // the original cosmos chain id
+	Cc         *lens.ChainClient
+
+	BridgeAddr    string // cosmos canonical hex addr
+	VaultAddr     string // cosmos canonical hex addr
+	PegBridgeAddr string // cosmos canonical hex addr
+
+	MsgPackage string // if provided, with replace the package of the Any typeUrl of the sdk.Msg
+}
+
+func NewCosClient(cfg *common.OneChainConfig, homeDir, keyringBackend, transactor_passphrase string) *CosClient {
+	if !commontypes.IsCosChain(cfg.ChainID) {
+		log.Fatalf("find invalid cosmos chainId:%d", cfg.ChainID)
+	}
+
+	ret := &CosClient{
+		ChainID:       cfg.ChainID,
+		RawChainID:    cfg.RawChainID,
+		BridgeAddr:    cfg.CBridge,
+		VaultAddr:     cfg.OTVault,
+		PegBridgeAddr: cfg.PTBridge,
+		MsgPackage:    cfg.MsgPackage,
+	}
+
+	if has0xPrefix(ret.BridgeAddr) {
+		ret.BridgeAddr = ret.BridgeAddr[2:] // requires no 0x in cosmos sdk
+	}
+	if has0xPrefix(ret.VaultAddr) {
+		ret.VaultAddr = ret.VaultAddr[2:]
+	}
+	if has0xPrefix(ret.PegBridgeAddr) {
+		ret.PegBridgeAddr = ret.PegBridgeAddr[2:]
+	}
+
+	chainClientConfig := &lens.ChainClientConfig{
+		Key:            cfg.Key,
+		ChainID:        cfg.RawChainID,
+		RPCAddr:        cfg.Gateway,
+		AccountPrefix:  cfg.AccountPrefix,
+		KeyringBackend: keyringBackend,
+		GasAdjustment:  cfg.GasAdjustment,
+		GasPrices:      cfg.GasPrices,
+		Debug:          true,
+		Timeout:        cfg.Timeout,
+		OutputFormat:   "json",
+		SignModeStr:    "direct",
+		Modules:        append([]module.AppModuleBasic{}, lens.ModuleBasics...),
+	}
+
+	reader := strings.NewReader(transactor_passphrase + "\n")
+	cc, err := lens.NewChainClient(chainClientConfig, homeDir, reader, os.Stdout)
+	if err != nil {
+		log.Fatalf("init chain client err: %s", err.Error())
+	}
+	err = cc.RPCClient.Start()
+	if err != nil {
+		log.Fatalf("start rpc client err: %s", err.Error())
+	}
+	ret.Cc = cc
+	return ret
+}
+
+func has0xPrefix(str string) bool {
+	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
+}
+
+func (c *CosClient) Monitor(eventQuery string, handleEvents func(events map[string][]string), outCapacity ...int) error {
+	res, err := c.Cc.RPCClient.Subscribe(context.Background(), "monitor", eventQuery, outCapacity...)
+
+	if err != nil {
+		log.Errorln("ws client subscribe error", err)
+		return err
+	}
+
+	for e := range res {
+		handleEvents(e.Events)
+	}
+
+	return nil
+}
+
+var (
+	EventQuery = tmquery.MustParse("tm.event='Tx'").String()
+
+	AndContractQueryParamTmpl = " AND wasm._contract_address='%s'"
+)
+
+func has(slice []string, target string) bool {
+	for _, s := range slice {
+		if s == target {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *CosClient) MonOTV(depositCallback, withdrawCallback, dtExectutedCallback func(events map[string][]string, chid uint64)) {
+	if c.VaultAddr == "" {
+		return
+	}
+	contractAddr, _ := cosmostypes.AccAddressFromHex(c.VaultAddr)
+	log.Infof("start monitor cosmos chain original_token_vault, chainid: %s", c.RawChainID)
+	err := c.Monitor(EventQuery+fmt.Sprintf(AndContractQueryParamTmpl, c.Cc.MustEncodeAccAddr(contractAddr)),
+		func(events map[string][]string) {
+			// todo: not sure if those three events would be strictly seperated by this monitor service, so parallel processing them at present.
+			// event: deposited
+			if has(events["wasm.action"], "deposit") && events["wasm.dst_chid"] != nil {
+				log.Infof("Mon commos chain deposited:%+v, chainid: %s", events, c.RawChainID)
+				depositCallback(events, c.ChainID)
+			}
+			// event: withdrawn
+			if has(events["wasm.action"], "withdraw") && events["wasm.ref_chain_id"] != nil {
+				log.Infof("Mon commos chain withdrawn:%+v, chainid: %s", events, c.RawChainID)
+				withdrawCallback(events, c.ChainID)
+			}
+			// event: delayed withdraw executed
+			if has(events["wasm.delayed_transfer_action"], "execute_delayed_transfer") && events["wasm.delayed_transfer_id"] != nil {
+				log.Infof("Mon commos chain delayed withdraw executed:%+v, chainid: %s", events, c.RawChainID)
+				dtExectutedCallback(events, c.ChainID)
+			}
+		})
+	if err != nil {
+		log.Fatalf("fail mon cosmos chain original_token_vault, err:%s", err.Error())
+	}
+}
+
+func (c *CosClient) MonPTB(burnCallback, mintCallback, dtExecutedCallback func(events map[string][]string, chid uint64)) {
+	if c.PegBridgeAddr == "" {
+		return
+	}
+	contractAddr, _ := cosmostypes.AccAddressFromHex(c.PegBridgeAddr)
+	log.Infof("start monitor cosmos chain pegged_token_bridge, chainid: %s", c.RawChainID)
+	err := c.Monitor(EventQuery+fmt.Sprintf(AndContractQueryParamTmpl, c.Cc.MustEncodeAccAddr(contractAddr)),
+		func(events map[string][]string) {
+			// todo: not sure if those three events would be strictly seperated by this monitor service, so parallel processing them at present.
+			// event: burn
+			if has(events["wasm.action"], "burn") && events["wasm.to_chid"] != nil {
+				log.Infof("Mon commos chain burn:%+v, chainid: %s", events, c.RawChainID)
+				burnCallback(events, c.ChainID)
+			}
+			// event: mint
+			if has(events["wasm.action"], "mint") && events["wasm.ref_chain_id"] != nil {
+				log.Infof("Mon commos chain mint:%+v, chainid: %s", events, c.RawChainID)
+				mintCallback(events, c.ChainID)
+			}
+			// event: delayed mint executed
+			if has(events["wasm.delayed_transfer_action"], "execute_delayed_transfer") && events["wasm.delayed_transfer_id"] != nil {
+				log.Infof("Mon commos chain delayed mint executed:%+v, chainid: %s", events, c.RawChainID)
+				dtExecutedCallback(events, c.ChainID)
+			}
+		})
+	if err != nil {
+		log.Fatalf("fail mon cosmos chain pegged_token_bridge, err:%s", err.Error())
+	}
+}
+
+func ToHex(b []byte) string {
+	length := len(b)
+	if length%2 == 1 {
+		length++ //word length in oven
+	}
+	if length == 20 {
+		return ec.BytesToAddress(b).Hex()
+	}
+	if length == 32 {
+		return ec.BytesToHash(b).Hex()
+	}
+	return "0x" + ec.Bytes2Hex(b)
+}
+
+func (c *CosClient) GetBlockTs() (time.Time, error) {
+	height, err := c.Cc.QueryLatestHeight()
+	if err != nil {
+		return time.Now(), err
+	}
+	header, err := c.Cc.QueryHeaderAtHeight(height)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	h, ok := header.(*tmclient.Header)
+	if ok {
+		return h.Header.Time, nil
+	} else {
+		return time.Now(), fmt.Errorf("fail to get terra cur blk ts")
+	}
+}
+
+func (c *CosClient) DelayTransferExist(id ec.Hash, contractCanonicalAddr string) (bool, error) {
+	contractAddr, err := c.GetContractHumanAddress(contractCanonicalAddr)
+	if err != nil {
+		return false, err
+	}
+	request := &commontypes.QuerySmartContractStateRequest{
+		Address:   contractAddr,
+		QueryData: []byte(fmt.Sprintf("{\"delayed_transfer\":{\"id\":\"%x\"}}", id)),
+	}
+	_, err = commontypes.SmartContractState(c.Cc, c.MsgPackage, request)
+	if err != nil {
+		if strings.Contains(err.Error(), "DelayedXfer not found") {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (c *CosClient) GetDelayThreshold(contractCanonicalAddr string) (int64, error) {
+	contractAddr, err := c.GetContractHumanAddress(contractCanonicalAddr)
+	if err != nil {
+		return 0, err
+	}
+	request := &commontypes.QuerySmartContractStateRequest{
+		Address:   contractAddr,
+		QueryData: []byte("{\"delay_period\":{}}"),
+	}
+	resp, err := commontypes.SmartContractState(c.Cc, c.MsgPackage, request)
+	if err != nil {
+		return 0, err
+	}
+	var period int64
+	err = json.Unmarshal(resp.Data, &period)
+	if err != nil {
+		return 0, err
+	}
+	return period, nil
+}
+
+func (c *CosClient) ExecuteDelay(id ec.Hash, contractCanonicalAddr string) (string, error) {
+	contractHumanAddr, err := c.GetContractHumanAddress(contractCanonicalAddr)
+	if err != nil {
+		log.Errorf("err:%v", err)
+		return "", err
+	}
+	senderAddr, err := c.Cc.Address()
+	if err != nil {
+		log.Errorf("err:%v", err)
+		return "", err
+	}
+	msg := &commontypes.MsgExecuteContract{
+		Sender:       senderAddr,
+		Contract:     contractHumanAddr,
+		ExecuteMsg:   []byte(fmt.Sprintf(`{"execute_delayed_transfer":{"id":"%x"}}`, id)),
+		Coins:        nil,
+		SenderPrefix: c.Cc.Config.AccountPrefix,
+	}
+	resp, err := c.Cc.SendMsgWithPackageName(context.Background(), msg, &c.MsgPackage)
+	if err != nil {
+		log.Errorf("err:%v", err)
+		return "", err
+	}
+	return resp.TxHash, nil
+}
+
+func (c *CosClient) GetContractHumanAddress(contractCanonicalAddr string) (string, error) {
+	addr, err := cosmostypes.AccAddressFromHex(c.VaultAddr)
+	if err != nil {
+		return "", err
+	}
+	return c.Cc.MustEncodeAccAddr(addr), nil
+}
---
+++ go-client/go.mod
@@ -0,0 +1,144 @@
+module github.com/celer-network/cbridge-cowa/go-client
+
+go 1.18
+
+replace (
+	github.com/btcsuite/btcd => github.com/btcsuite/btcd v0.22.1 // to fix ambiguous import
+	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+	github.com/strangelove-ventures/lens => github.com/celer-network/lens v0.0.0-20220317113705-caea22530051
+)
+
+require (
+	github.com/celer-network/goutils v0.1.56
+	github.com/celer-network/sgn-v2 v0.0.0-20221025071050-74f157b4cebf
+	github.com/cosmos/cosmos-sdk v0.45.4
+	github.com/cosmos/ibc-go/v2 v2.0.2
+	github.com/ethereum/go-ethereum v1.10.21
+	github.com/strangelove-ventures/lens v0.2.1
+	github.com/tendermint/tendermint v0.34.19
+)
+
+require (
+	filippo.io/edwards25519 v1.0.0-beta.2 // indirect
+	github.com/99designs/keyring v1.1.6 // indirect
+	github.com/ChainSafe/go-schnorrkel v0.0.0-20200405005733-88cbf1b4c40d // indirect
+	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/VictoriaMetrics/fastcache v1.6.0 // indirect
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/avast/retry-go v2.6.0+incompatible // indirect
+	github.com/aws/aws-sdk-go v1.42.23 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bgentry/speakeasy v0.1.0 // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/celer-network/cbridge-flow v0.0.5 // indirect
+	github.com/celer-network/sgn-proxy/rpc v0.0.0-20221025023446-f4e193fa4661 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/confio/ics23/go v0.6.6 // indirect
+	github.com/cosmos/btcutil v1.0.4 // indirect
+	github.com/cosmos/go-bip39 v1.0.0 // indirect
+	github.com/cosmos/iavl v0.17.3 // indirect
+	github.com/cosmos/ledger-cosmos-go v0.11.1 // indirect
+	github.com/cosmos/ledger-go v0.9.2 // indirect
+	github.com/cosmos/relayer v1.0.1-0.20220107210018-e85b7af9a30b // indirect
+	github.com/danieljoos/wincred v1.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
+	github.com/dgraph-io/ristretto v0.0.3 // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/dvsekhvalnov/jose2go v0.0.0-20200901110807-248326c1351b // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-kit/kit v0.12.0 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
+	github.com/gogo/protobuf v1.3.3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
+	github.com/gtank/merlin v0.1.1 // indirect
+	github.com/gtank/ristretto255 v0.1.2 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hdevalence/ed25519consensus v0.0.0-20210204194344-59a8610d2b87 // indirect
+	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
+	github.com/holiman/uint256 v1.2.0 // indirect
+	github.com/improbable-eng/grpc-web v0.15.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jmhodges/levigo v1.0.0 // indirect
+	github.com/keybase/go-keychain v0.0.0-20190712205309-48d3d31d256d // indirect
+	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/mimoo/StrobeGo v0.0.0-20181016162300-f8f6d4d2b643 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/mtibben/percent v0.2.1 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/prometheus/tsdb v0.10.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/regen-network/cosmos-proto v0.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/cobra v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.12.0 // indirect
+	github.com/stretchr/testify v1.7.2 // indirect
+	github.com/subosito/gotenv v1.3.0 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
+	github.com/tendermint/btcd v0.1.1 // indirect
+	github.com/tendermint/crypto v0.0.0-20191022145703-50d29ede1e15 // indirect
+	github.com/tendermint/go-amino v0.16.0 // indirect
+	github.com/tendermint/tm-db v0.6.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.7 // indirect
+	github.com/tklauser/numcpus v0.2.3 // indirect
+	github.com/zondax/hid v0.9.0 // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.8 // indirect
+	google.golang.org/genproto v0.0.0-20221014213838-99cd37c6964a // indirect
+	google.golang.org/grpc v1.50.1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
